7c3f154d4309932c1ebbb85f2df7692973aff226
Pirate <memesdudeguy@protonmail.com>
2025-05-10 09:31:13 -0700

    Delete UniOS directory

--- UniOS/drivers/linux_drivers.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "linux_drivers.h"
-
-namespace Drivers {
-    void initializeDrivers() {
-        std::cout << "[Drivers] Initializing Linux drivers..." << std::endl;
-        // Placeholder: Load and initialize Linux drivers
-    }
-}
--- UniOS/drivers/linux_drivers.h
+++
@@ -1,9 +0,0 @@
-#ifndef UNI_OS_LINUX_DRIVERS_H
-#define UNI_OS_LINUX_DRIVERS_H
-
-namespace Drivers {
-    // Initialize Linux drivers
-    void initializeDrivers();
-}
-
-#endif // UNI_OS_LINUX_DRIVERS_H
